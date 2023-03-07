bb721a119f9298b40800c0d68ed4bd0b61f1f8d8
Yana Buyankina <yana.buyanckina@yandex.ru>
2023-03-07 14:23:06 +0300

    Создано минимальное приложение (blinky)

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,16 @@
-int main () {
+#include <libopencm3/stm32/rcc.h>
+#include <libopencm3/stm32/gpio.h>
 
-    while (true) {
+int main ()
+{
 
+    rcc_periph_clock_enable(RCC_GPIOE);
+
+    gpio_mode_setup(GPIOE, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO9);
+
+    while (true)
+    {
+        gpio_toggle(GPIOE, GPIO9);
     }
 
 }
