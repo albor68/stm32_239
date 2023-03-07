9be67c529a420ea7775b9ba758d5e10c86795947
LEX <13.pallex.13@gmail.com>
2023-03-07 14:24:38 +0300

    Создано минимальное приложение (blinky), исправлен CMakeLists.txt

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,13 @@
+#include <libopencm3/stm32/rcc.h>
+#include <libopencm3/stm32/gpio.h>
+
 int main () {
 
-    while (true) {
+    rcc_periph_clock_enable(RCC_GPIOE);
+    gpio_mode_setup(GPIOE,GPIO_MODE_OUTPUT,GPIO_PUPD_NONE,GPIO9);
 
+    while (true) {
+        gpio_toggle(GPIOE, GPIO9);
     }
 
 }
