050a0ce4080abcc9abdf11ea540a4a01a29792b9
denisdo <den.dorkin@mail.ru>
2023-03-21 13:49:58 +0300

    Вторая пара 21.03.23

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,9 @@
+#include <libopencm3/stm32/rcc.h>
+#include <libopencm3/stm32/gpio.h>
 int main () {
-
+rcc_periph_clock_enable(RCC_GPIOE);
+gpio_mode_setup(GPIOE,GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO9|GPIO11|GPIO13|GPIO15);
     while (true) {
-
+gpio_toggle(GPIOE,GPIO9|GPIO11|GPIO13|GPIO15);
     }
-
 }
