ef149decee1ac61b9702a308fa75426b4f4a193e
Yana Buyankina <yana.buyanckina@yandex.ru>
2023-03-21 13:49:59 +0300

    Вторая пара 21 марта (горят все светодиоды)

--- src/main.cpp
+++ src/main.cpp
@@ -6,11 +6,11 @@ int main ()
 
     rcc_periph_clock_enable(RCC_GPIOE);
 
-    gpio_mode_setup(GPIOE, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO9 | GPIO11);
+    gpio_mode_setup(GPIOE, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO8 | GPIO9 | GPIO10 | GPIO11 | GPIO12 |GPIO13 | GPIO14 | GPIO15);
 
     while (true)
     {
-        gpio_toggle(GPIOE, GPIO9 | GPIO11);
+        gpio_toggle(GPIOE, GPIO8 | GPIO9 | GPIO10 | GPIO11 | GPIO12 |GPIO13 | GPIO14 | GPIO15);
     }
 
 }
