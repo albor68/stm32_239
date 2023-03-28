b7b4891640cd430a2b54683d0a4681518b1518dc
denisdo <den.dorkin@mail.ru>
2023-03-28 15:07:02 +0300

    Рефакторинг(Refactoring)

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,24 @@
 #include <libopencm3/stm32/rcc.h>
 #include <libopencm3/stm32/gpio.h>
+
+void gpio_setup();
+void gpio_use();
+
 int main () {
-rcc_periph_clock_enable(RCC_GPIOE);
-gpio_mode_setup(GPIOE,GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO9|GPIO11|GPIO13|GPIO15);
+gpio_setup ();
+
+
     while (true) {
-gpio_toggle(GPIOE,GPIO9|GPIO11|GPIO13|GPIO15);
+
+gpio_use();
     }
+}
+
+void gpio_setup(){
+    rcc_periph_clock_enable(RCC_GPIOE);
+    gpio_mode_setup(GPIOE,GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO9|GPIO11|GPIO13|GPIO12);
+}
+void gpio_use (){
+
+gpio_toggle(GPIOE,GPIO9|GPIO11|GPIO13|GPIO15);
 }
