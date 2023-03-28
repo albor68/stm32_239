525b985eeeea21689e1466ac2bf964f5d906746e
Ilya <gubanovilja2013@gmail.com>
2023-03-28 15:06:42 +0300

    Рефакторинг

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,26 @@
 #include <libopencm3/stm32/rcc.h>
 #include <libopencm3/stm32/gpio.h>
 
+void gpio_setup();
+void gpio_use();
+
 int main () {
-
-    rcc_periph_clock_enable(RCC_GPIOE);
-
-    gpio_mode_setup(GPIOE, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO9|GPIO11);
+    gpio_setup();
 
 while (true) {
-
-    gpio_toggle(GPIOE, GPIO9|GPIO11);
+    gpio_use();
 
     }
+}
+
+void gpio_setup(){
+   rcc_periph_clock_enable(RCC_GPIOE);
+
+   gpio_mode_setup(GPIOE, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO9|GPIO11);
+}
+
+void gpio_use(){
+
+gpio_toggle(GPIOE, GPIO9|GPIO11);
+
 }
