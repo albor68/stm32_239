1913961856a21424f2ca74f04d814cebef0ea3ee
Yana Buyankina <yana.buyanckina@yandex.ru>
2023-03-28 15:06:43 +0300

    Рефакторинг

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,29 @@
 #include <libopencm3/stm32/rcc.h>
 #include <libopencm3/stm32/gpio.h>
 
+void gpio_setup();
+
+void gpio_use();
+
 int main ()
 {
-
-    rcc_periph_clock_enable(RCC_GPIOE);
-
-    gpio_mode_setup(GPIOE, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO8 | GPIO9 | GPIO10 | GPIO11 | GPIO12 |GPIO13 | GPIO14 | GPIO15);
+    gpio_setup();
 
     while (true)
     {
-        gpio_toggle(GPIOE, GPIO8 | GPIO9 | GPIO10 | GPIO11 | GPIO12 |GPIO13 | GPIO14 | GPIO15);
+        gpio_use();
     }
 
+}
+
+void gpio_setup()
+{
+    rcc_periph_clock_enable(RCC_GPIOE);
+    gpio_mode_setup(GPIOE, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO8 | GPIO9 | GPIO10 | GPIO11 | GPIO12 |GPIO13 | GPIO14 | GPIO15);
+
+}
+
+void gpio_use()
+{
+     gpio_toggle(GPIOE, GPIO8 | GPIO9 | GPIO10 | GPIO11 | GPIO12 |GPIO13 | GPIO14 | GPIO15);
 }
