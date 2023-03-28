4efa6b761c017166f5ea8c1dc47beca6d10460f6
LEX <13.pallex.13@gmail.com>
2023-03-28 15:07:21 +0300

    Рефакторинг преложения

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,26 @@
 #include <libopencm3/stm32/rcc.h>
 #include <libopencm3/stm32/gpio.h>
 
+void gipo_setup();
+void gipo_use();
+
 int main () {
 
-    rcc_periph_clock_enable(RCC_GPIOE);
-    gpio_mode_setup(GPIOE,GPIO_MODE_OUTPUT,GPIO_PUPD_NONE,GPIO9|GPIO13);
+    gipo_setup();
 
     while (true) {
-        gpio_toggle(GPIOE, GPIO9|GPIO13);
+
+        gipo_use();
+
     }
 
+}
+
+void gipo_setup(){
+    rcc_periph_clock_enable(RCC_GPIOE);
+    gpio_mode_setup(GPIOE,GPIO_MODE_OUTPUT,GPIO_PUPD_NONE,GPIO10|GPIO12);
+}
+
+void gipo_use(){
+    gpio_toggle(GPIOE, GPIO10|GPIO12);
 }
